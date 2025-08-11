741bb2bb58f19db0441f059b2eccf703d638b03e
YUJINDL01 <qkrdbwlsdl01@gmail.com>
2025-08-11 13:58:10 +0900

    Update PlayerInteraction.cs

--- Magic/Assets/Scirpts/Player/PlayerInteraction.cs
+++ Magic/Assets/Scirpts/Player/PlayerInteraction.cs
@@ -1,38 +1,59 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    public float interactRange = 3f;
-    public string interactText;
+    public Camera mainCamera;
+    public float maxDistance = 100f;
 
-    private IInteractable currentTarget;
+    // private PlayerInputActions inputActions; // 지금 이게 어딨는 지 모름 
+    // 클릭에 vr 관련 행동 추가하고 
+    // 움직임이 있는 스크립트나 따로 빼면 됨
 
-    void Update()
+    private void Awake()
     {
-        DetectInteractable();
+        if (mainCamera == null)
+            mainCamera = Camera.main; // 이 부분 나중에 멀티로 바뀌면 오류가 좀 날 수도?? 
 
-        if (Input.GetKeyDown(KeyCode.E) && currentTarget != null)
-        {
-            currentTarget.Interact();
-        }
+        //inputActions = new PlayerInputActions();
     }
 
-    void DetectInteractable()
+    private void OnEnable()
     {
-        Ray ray = new Ray(transform.position, transform.forward);
-        if (Physics.Raycast(ray, out RaycastHit hit, interactRange))
+        //inputActions.Player.Enable();
+        //inputActions.Player.Click.performed += OnClickPerformed;
+    }
+
+    private void OnDisable()
+    {
+        //inputActions.Player.Click.performed -= OnClickPerformed;
+        //inputActions.Player.Disable();
+    }
+
+    private void OnClickPerformed(InputAction.CallbackContext context)
+    {
+        
+        Debug.Log("Click 했음");
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (Physics.Raycast(ray, out RaycastHit hit, maxDistance))
         {
+            Debug.Log($"{hit.collider.gameObject.name}입니다."); // 이 부분을 이름이 아니라 상호작용 가능한 객체인지 확인하고 해당 사항 반환하게 하기
+            
             IInteractable interactable = hit.collider.GetComponent<IInteractable>();
             if (interactable != null)
             {
-                currentTarget = interactable;
-                //interactText.text = interactable.GetInteractText();
-                //interactText.gameObject.SetActive(true);
-                return;
+                HandleInteraction(interactable, hit.collider.gameObject);
             }
         }
+    }
 
-        currentTarget = null;
-        //interactText.gameObject.SetActive(false);
+    private void HandleInteraction(IInteractable interactable, GameObject target)
+    {   
+        Debug.Log("상호작용 한다잉");
+        // 추후 뭔가가 추가 되면 여기서 어떤 행동을 하지 등 분기 나뉘면 될 듯
+        interactable.Interact();
     }
 }
